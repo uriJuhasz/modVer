89f211ab4a51a7003f60336edd99fef5230065cd
uri.juhasz@gmail.com <uri.juhasz@gmail.com>
2016-12-07 02:45:21 +0100

    ...

--- src/common/data_types.h
+++ src/common/data_types.h
@@ -1,11 +1,12 @@
 #ifndef COMMON__DATA_TYPES_H
 #define COMMON__DATA_TYPES_H
 #include <string>
+#include <boost/multiprecision/cpp_int.hpp>
 
 namespace common{
     typedef wchar_t     Char;
-    typedef long int    Integer;
-    typedef long double Rational;
+    typedef boost::multiprecision::cpp_int      Integer;
+    typedef boost::multiprecision::cpp_rational Rational;
     typedef bool        Boolean;
     typedef std::wstring String;
     typedef std::wstring ID;
