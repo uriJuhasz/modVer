a34a021af05d9296efb361ef6fa90a6d3bfd5be5
uri.juhasz@gmail.com <uri.juhasz@gmail.com>
2016-12-14 19:28:59 +0100

    ...

--- src/frontend/boogie/AST.h
+++ src/frontend/boogie/AST.h
@@ -14,133 +14,301 @@ namespace AST{
     using common::String;
     using String = common::String;
     using frontend::TextPosition;
+    
+    using namespace std;
 
     using std::unique_ptr;
     using std::vector;
-    using std::list;
     using std::string;
+    using std::list;
 
+    // <editor-fold desc="utilities">
+    template<typename T,template<U> C>list<T>&& clone(const C<T>& in){
+        C<T> out;
+        for (const auto& e : in)
+            out.push_back(e->clone());
+        return out;
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="ASTNode">
     class ASTNode{
     public:
     	TextPosition textPos;
     protected:
-    	ASTNode(const TextPosition& _textPos = TextPosition::NoPos) : textPos(_textPos){}
+    	ASTNode(const TextPosition& _textPos = TextPosition::NoPos) 
+                : textPos(_textPos){}
     };
-    class AttributesAndTriggers;
+    // </editor-fold>
+    
+    // <editor-fold desc="forwards">
+    class Attribute;
+    typedef unique_ptr<Attribute> pAttribute;
+    typedef vector<pAttribute> Attributes;
+    class Trigger;
+    typedef unique_ptr<Trigger> pTrigger;
+    typedef vector<pTrigger>   Triggers;
+    
     class Expression;
-    class TypeVariable : public ASTNode{};
-    class Type : public ASTNode{public:unique_ptr<Attributes> clone() const;};
+    typedef unique_ptr<Expression> pExpression;
+    // </editor-fold>
+    
+    // <editor-fold desc="Identifiers">
+    /////////////////////////////////////////////////////////////
+    //Identifier
+    class Identifier;
+    typedef unique_ptr<Identifier> pIdentifier;
     class Identifier : public ASTNode{
     public:
-        Identifier(const common::String& _name)
-        : name(_name){}
+        Identifier(TextPosition _pos, const common::String& _name)
+        : ASTNode(_pos), name(_name){}
         String name;
     };
-    class TypeDef  : public ASTNode{};
+    // </editor-fold>
+    
+    // <editor-fold desc="Types">
+    /////////////////////////////////////////////////////////////
+    //Types
+    class Type : public ASTNode{
+    public:
+        pType clone() const = 0;
+        virtual ~Type() = 0;
+    };
+    typedef unique_ptr<Type> pType;
+
+    class TypeVariable : public ASTNode{
+    public:
+        TypeVariable(pIdentifier&& _id) : id(_id){}
+        pIdentifier id;
+    };
+    typedef unique_ptr<TypeVariable> pTypeVariable;
+    typedef list<pTypeVariable> TypeParameters;
+    
+    class TypeConstructorInstance : public Type{
+    public:
+        TypeConstructorInstance(pIdentifier&& _id, list<pType>&& _arguments)
+            : id(_id), arguments(_arguments)
+        {}
+            
+        pIdentifier id;
+        list<pType> arguments;
+    };
+    class MapType : public Type{
+    public:
+        MapType(TypeParameters&& _typeParameters, vector<pType>&& _argumentTypes, pType&& _resultType)
+                : TypeParameters(_typeParameters), argumentTypes(_argumentTypes), resultType(_resultType)
+        {}
+        TypeParameters typeParameters;
+        vector<pType> argumentTypes;
+        pType resultType; 
+    };
+    class VariableType : public Type{
+    public:
+        VariableType(pTypeVariable&& _var)
+            : var(_var){}
+        pTypeVariable var;
+    };
+
+    class TypeDef  : public ASTNode{
+    public: 
+        TypeDef(pIdentifier&& _id, TypeParameters&& _parameters)
+            : id(_id), parameters(_parameters){}
+        virtual ~TypeDef() = 0;
+        pIdentifier id;
+        TypeParameters parameters;
+    };
+    typedef unique_ptr<TypeDef> pTypeDef;
+    
+    class TypeConstructorDef : public TypeDef{
+    public:
+        TypeConstructorDef(pIdentifier&& _id, TypeParameters&& _parameters)
+            : TypeDef(_id,_parameters){}
+    };
+    class TypeSynonymDef : public TypeDef{
+    public:
+        TypeSynonymDef(pIdentifier&& _id, TypeParameters&& _parameters, pType&& _target)
+            : TypeDef(_id,_parameters), target(_target){}
+        pType target;
+    };
+    // </editor-fold>
+
+    // <editor-fold desc="Variables">
+    /////////////////////////////////////////////////////////////
+    //Variables
     class Variable : public ASTNode{
     public:
-    	Variable(
-    			const TextPosition& _textPos,
-				unique_ptr<Attributes> _attributes,
-				const Identifier& _id,
-				unique_ptr<Type> _type)
-    		: ASTNode(_textPos), name(_id.name), type(move(_type)), attributes(move(_attributes))
-    	{}
+        static constexpr int GLOBAL   = 1 << 0;
+        static constexpr int INPUT    = 1 << 1;
+        static constexpr int OUTPUT   = 1 << 2;
+        static constexpr int CONSTANT = 1 << 3;
+    	Variable(const TextPosition _textPos,
+                 Attributes&& _attributes,
+                 pIdentifier&& _id,
+                 pType&& _type,
+                 int _flags)
+    		: ASTNode(_textPos), 
+                  id(move(_id)), 
+                  type(move(_type)), 
+                  attributes(move(_attributes)),
+                  flags(_flags)
+            {}
     	virtual ~Variable(){}
-    	String name;
-    	unique_ptr<Type>       type;
-    	unique_ptr<Attributes> attributes;
-    	virtual bool isGlobal()=0;
+    	pIdentifier id;
+    	pType       type;
+    	Attributes attributes;
+        
+        int flags;
+        bool isGlobal(){return flags&GLOBAL; }
+        bool isInput() {return flags&INPUT; }
+        bool isOutput(){return flags&OUTPUT; }
+        bool isConstant(){return flags&CONSTANT; }
     };
-    	class ConstantParentSpec;
-    	class Constant : public Variable{
-    	public:
-    		Constant(
-    			const TextPosition& _textPos,
-				unique_ptr<Attributes> _attributes,
-				const Identifier& _id,
-				unique_ptr<Type> _type,
-				bool _isUnique,
-				unique_ptr<ConstantParentSpec> _parentSpec)
-    			: Variable(_textPos,move(_attributes),_id,move(_type)), isUnique(_isUnique), parentSpec(move(_parentSpec))
-    		{}
-    		virtual ~Constant(){}
-    		bool isUnique;
-    		unique_ptr<ConstantParentSpec> parentSpec;
-        	bool isGlobal(){return true;}
-    	};
-        class GlobalVariable : public Variable{
-        public:
-        	GlobalVariable(
-        			const TextPosition& _textPos,
-    				unique_ptr<Attributes> _attributes,
-    				const Identifier& _id,
-    				unique_ptr<Type> _type)
-        		: Variable(_textPos,move(_attributes),_id, move(_type))
-        	{}
-        	virtual ~GlobalVariable(){}
-        	String name;
-        	unique_ptr<Type>       type;
-        	unique_ptr<Attributes> attributes;
-        	bool isGlobal(){return true;}
-        };
-
-    class FunctionDef : public ASTNode{
+    typedef unique_ptr<Variable> pVariable;
+    class ConstantParentSpec;
+    typedef unique_ptr<ConstantParentSpec> pConstantParentSpec;
+    class Constant final : public Variable{
     public:
-        FunctionDef(
-        	const TextPosition& _textPos,
-			unique_ptr<Attributes> _attributes,
-			unique_ptr<Identifier> _name,
-			vector<TypeVariable>& _typeParameters,
-			vector<Variable>& _parameters,
-			unique_ptr<Variable> _returnVar,
-			unique_ptr<Expression> _body)
-			: ASTNode(_textPos),
-			  attributes(move(_attributes)),
-    		  name      (move(_name)),
-    		  typeParameters(move(_typeParameters)),
-    		  parameters (move(_parameters)),
-			  returnVar (move(_returnVar)),
-			  body      (move(_body))
-		{}
-        unique_ptr<Attributes> attributes;
-        unique_ptr<Identifier> name;
-		vector<TypeVariable> typeParameters;
-		vector<Variable> parameters;
-		unique_ptr<Variable> returnVar;
-		unique_ptr<Expression> body;
+        Constant(const TextPosition& _textPos,
+                 Attributes&&  _attributes,
+                 pIdentifier&& _id,
+                 pType&&       _type,
+                 bool          _isUnique,
+                 pConstantParentSpec _parentSpec)
+                : Variable(
+                   _textPos,
+                   move(_attributes),
+                   move(_id),
+                   move(_type),
+                   Variable::GLOBAL),
+                   isUnique(_isUnique), 
+                   parentSpec(move(_parentSpec))
+        {}
+        bool isUnique;
+        pConstantParentSpec parentSpec;
     };
-
     class ConstantParentSpec: public ASTNode {
     public:
-    	bool unique;
-
+        ConstantParentSpec(pIdentifier&& _id, bool _isUnique)
+            : id(_id), isUnique(_isUnique)
+            {}
+        bool isUnique;
+        pIdentifier id;
     };
+    typedef unique_ptr<pConstantOrderSpec> pConstantOrderSpec;
     class ConstantOrderSpec : public ASTNode {
     	public:
     		bool specified = false;
     		bool ChildrenComplete = false;
     		vector<ConstantParentSpec> parents;
-    		unique_ptr<ConstantOrderSpec> clone() const;
+    		pConstantOrderSpec clone() const;
     };
+    // </editor-fold>
 
-
-
-    class Axiom          : public ASTNode{};
-    class Procedure      : public ASTNode{};
-    class Implementation : public ASTNode{};
-    
-    class Attribute;
-    class AttributeParam;
-    class Attributes : public ASTNode{
+    // <editor-fold desc="Functions">
+    class Function : public ASTNode{
     public:
-        unique_ptr<Attributes> clone() const;
-        list<unique_ptr<Attribute>> attributes;
+        Function(
+        	const TextPosition& _textPos,
+            Attributes&&      _attributes,
+            pIdentifier&&     _id,
+            TypeParameters&&  _typeParameters,
+            list<pVariable>&& _parameters,
+            pVariable&&       _returnVar,
+            pExpression       _body)
+            : ASTNode(_textPos),
+              attributes    (_attributes),
+              id            (_id),
+              typeParameters(_typeParameters),
+    	      parameters    (_parameters),
+    	      returnVar     (_returnVar),
+              body          (_body)
+            {}
+        Attributes      attributes;
+        pIdentifier     id;
+        TypeParameters  typeParameters;
+        list<pVariable> parameters;
+        pVariable       returnVar;
+        pExpression     body;
     };
+    typedef unique_ptr<FunctionDef> pFunction;
+    // </editor-fold>
+
+    // <editor-fold desc="Axioms">
+    class Axiom : public ASTNode{
+    public:
+        Axiom(Attributes&& _attributes, pExpression&& _expression)
+            : attributes(_attributes), expression(_expression)
+        {}
+        Attributes attributes;
+        pExpression expression;
+    };
+    typedef unique_ptr<Axiom> pAxiom;
+    // </editor-fold>
+    
+    // <editor-fold desc="Procedures">
+    class pVariableExpression;
+    class SpecExpression : public ASTNode{
+    public:
+        SpecExpression(pExpression&& _e, bool _isFree)
+            : e(_e), isFree(_isFree){}
+        pExpression e;
+        bool isFree;
+    };
+    
+    typedef unique_ptr<SpecExpression> pSpecExpression;
+    class ProcSignature : public ASTNode{
+        vector<pVariable> inputs;
+        vector<pVariable> outputs;
+
+        vector<pVariableExpression> modifies;
+        vector<pSpecExpression> preconditions;
+        vector<pSpecExpression> postconditions;
+    };
+    typedef unique_ptr<ProcSignature> pProcSignature;
+    
+    class ProcedureSC : public ASTNode{
+    protected:
+        ProcedureSC(pIdentifier&& _id, Attributes&& _attributes, pProcSignature&& _sig)
+            : id(_id), attributes(_attributes), sig(_sig) {}
+    public:
+        virtual ~ProcedureSC() = 0;
+        pIdentifier id;
+        Attributes attributes;
+        ProcSignature sig;
+    };
+    class Procedure : public ProcedureSC{
+        Procedure(pIdentifier&& _id, Attributes&& _attributes, pProcSignature&& _sig)
+            : ProcedureSC( _id, _attributes, _sig) {}
+    };
+    typedef unique_ptr<Procedure> pProcedure;
+    
+    class Statement;
+    typedef unique_ptr<Statement> pStatement;
+    typedef vector<Variable> pLocals;
+    
+    class Implementation : public ASTNode{
+        Implementation(
+            pIdentifier&& _id, Attributes&& _attributes, pProcSignature&& _sig, vector<pVariable>&& pLocals, pStatement&& _body)
+            : ProcedureSC( _id, _attributes, _sig), locals(_locals), body(_body) {}
+        pLocals locals;
+        pStatement body;
+    };
+    typedef unique_ptr<Implementation> pImplementation;
+    // </editor-fold>
+    
+    // <editor-fold desc="Attributes and triggers">
+    class Attribute;
+    typedef vector<Attribute> Attributes;
+
+    class Trigger;
+    typedef vector<Trigger>   Triggers;
+
+    class AttributeParam;
+    typedef unique_ptr<AttributeParam> pAttributeParam;
     class Attribute : public ASTNode{
     public:
-        unique_ptr<Identifier> id;
-        list<unique_ptr<AttributeParam>> params;
+        pIdentifier id;
+        list<pAttributeParam> params;
     };
     class AttributeParam : public ASTNode{
     public:
@@ -148,48 +316,69 @@ namespace AST{
     };
     class StringAttributeParam : public AttributeParam{
     public:
+        public StringAttributeParam(const string& _value)
+            : value(_value){}
         std::string value;
     };
     class ExpressionAttributeParam : public AttributeParam{
     public:
-        unique_ptr<Expression> value;
+        ExpressionAttributeParam(pExpression _value)
+            : value(_value){}
+        pExpression value;
     };
-
-    class TypedIdentifier : public Identifier{ 
-    public: 
-        TypedIdentifier(const string& _id, unique_ptr<Type> _type);
-        unique_ptr<Type> type; 
+    class Trigger : public ASTNode{
+    public:
+        Trigger(vector<pExpression>&& _ts)
+            :ts(_ts){}
+        vector<pExpression> ts;
     };
+    // </editor-fold>
 
-    class ProcedureSignature : public ASTNode{};
-    class ProcedureSpec : public ASTNode{};
-    class ProcedureBody : public ASTNode{};
-    class TypeArguments : public ASTNode{};
-
+    // <editor-fold desc="Programs">
     class Scope : public ASTNode{
     public:
-    	void addTypeDef    (unique_ptr<TypeDef> td);
-    	void addVariableDef(unique_ptr<Variable> vd);
-    	void addFunctionDef(unique_ptr<FunctionDef> fd);
+    	void addTypeDef    (pTypeDef&&  d);
+    	void addVariableDef(pVariable&& d);
+    	void addFunctionDef(pFunction&& d);
     private:
-    	vector<Variable> variables;
-    	vector<TypeDef>     typeDefs;
-    	vector<FunctionDef> functions;
+    	vector<pVariable> variables;
+    	vector<pTypeDef>  typeDefs;
+    	vector<pFunction> functions;
     };
+
     class Program : public Scope{
     public:
-    	void addAxiom(unique_ptr<Axiom> axiom);
-    	void addProcedure(unique_ptr<Procedure> procedure);
-    	void addImplementation(unique_ptr<Implementation> Implementation);
+    	void addAxiom         (pAxiom&&          axiom);
+    	void addProcedure     (pProcedure&&      procedure);
+    	void addImplementation(pImplementation&& Implementation);
     private:
-        vector<Axiom>          axioms;
-        vector<Procedure>      procedures;
-        vector<Implementation> implementations;
+        vector<pAxiom>          axioms;
+        vector<pProcedure>      procedures;
+        vector<pImplementation> implementations;
     };
-    class Operation : public ASTNode{};
+//    class Operation : public ASTNode{};
+    // </editor-fold>
+
+    // <editor-fold desc="Expressions">
     class Expression : public ASTNode{
-    	static void make(const TextPosition& _textPos,const Operation op, std::initializer_list<unique_ptr<Expression>>);
+    public:
+        virtual ~Expression() = 0;
+        static void make(const TextPosition& _textPos,const Operation op, std::initializer_list<unique_ptr<Expression>>);
     };
+    
+    class Operation{};
+    typedef unique_ptr<Operation> pOperation;
+    class FAExpression : public Expression{
+    public:
+        FAExpression(pOperation&& _op, vector<pExpression>&& args)
+            : op(_op), args(_args){}
+        pOperation op;
+        vector<pExpression> args;
+    };
+    // </editor-fold>
+
+    // <editor-fold desc="Statements">
+    // </editor-fold>
 
 }//namespace AST
 }//namespace frontend
